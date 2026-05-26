4ce445bc74a13fd513e5d9cacf5f8d4a0b0e16be
HalfSilver <69383504+Half-Silver@users.noreply.github.com>
2026-05-26 20:24:38 +0530

    Initial commit for all-dev-nextcloud

---
+++ tests/integration/enable_https_spec.rb
@@ -0,0 +1,16 @@
+feature "Enabling HTTPS" do
+	after(:all) do
+		disable_https
+	end
+
+	scenario "self-signed" do
+		enable_https
+
+		visit "/"
+		fill_in "user", with: "admin"
+		fill_in "password", with: "admin"
+		click_button "Log in"
+		sleep 3
+		expect(page).to have_content /(Recommended|All) files/
+	end
+end
---
+++ tests/integration/login_spec.rb
@@ -0,0 +1,19 @@
+feature "Logging in" do
+	scenario "Logging in with correct credentials" do
+		visit "/"
+		fill_in "user", with: "admin"
+		fill_in "password", with: "admin"
+		click_button "Log in"
+		sleep 3
+		expect(page).to have_content /(Recommended|All) files/
+	end
+
+	scenario "Logging in with incorrect credentials" do
+		visit "/"
+		fill_in "user", with: "wronguser"
+		fill_in "password", with: "wrongpassword"
+		click_button "Log in"
+		sleep 3
+		expect(page).to have_content /Wrong.*password/
+	end
+end
